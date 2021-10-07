77a8f594a5967fa1fe073bfa441f5c54087cbb75
Anna Hakobyan <Anna.Hakobyan@helpsystems.com>
2021-10-08 00:06:47 +0400

    Add project files.

---
+++ Models/Cars.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace EventFirstLook.Models
+{
+    // With this example, you have configured the car to send two custom events without having to define custom
+    // registration functions or declare delegate member variables.
+
+    // Defining an event is a two-step process. First, you need to define a delegate type (or reuse an existing one)
+    // that will hold the list of methods to be called when the event is fired. Next, you declare an event
+    // (using the C# event keyword) in terms of the related delegate type.
+    public class Car
+    {
+        // Internal state data.
+        public int CurrentSpeed { get; set; }
+        public int MaxSpeed { get; set; } = 100;
+        public string PetName { get; set; }
+
+        // Is the car available or sold out?
+        private bool _carIsSoldOut;
+
+        // Class constructors.
+        public Car() { }
+        public Car(string name, int maxSp, int currSp)
+        {
+            CurrentSpeed = currSp;
+            MaxSpeed = maxSp;
+            PetName = name;
+        }
+
+        // This delegate works in conjunction with the Car's events.
+        public delegate void CarEngineHandler(string msgForCaller);
+
+        // This car can send these events.
+        public event CarEngineHandler Exploded;
+        public event CarEngineHandler AboutToBlow;
+
+        // Just fire out the Exploded notification.
+        public void Accelerate(int delta)
+        {
+            // If the car is sold out, fire Exploded event.
+            if (_carIsSoldOut)
+            {
+                Exploded?.Invoke("Sorry, this car is sold out...");
+            }
+            else
+            {
+                CurrentSpeed += delta;
+                // Almost sold out?
+                if (10 == MaxSpeed - CurrentSpeed)
+                {
+                    AboutToBlow?.Invoke("Careful buddy! Gonna blow!");
+                }
+
+                // Still OK!
+                if (CurrentSpeed >= MaxSpeed)
+                {
+                    _carIsSoldOut = true;
+                }
+                else
+                {
+                    Console.WriteLine("CurrentSpeed = {0}", CurrentSpeed);
+                }
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using EventFirstLook.Models;
+using System;
+
+namespace EventFirstLook
+{
+    // Events enable a class or object to notify other classes or objects when something of interest occurs.
+
+    // An event is dependent on a delegate and cannot be created without delegates.
+    // Event is a wrapper around delegate instance to prevent users of the delegate
+    // from resetting the delegate and its invocation list and
+    // only allows adding or removing targets from the invocation list.
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car c1 = new Car("SlugBug", 100, 10);
+
+            // Register event handlers.
+            c1.AboutToBlow += CarIsAlmostDoomed;
+            c1.AboutToBlow += CarAboutToBlow;
+
+            Car.CarEngineHandler d = CarExploded;
+            c1.Exploded += d;
+
+            Console.WriteLine("***** Speeding up *****");
+            for (int i = 0; i < 6; i++)
+            {
+                c1.Accelerate(20);
+            }
+
+            // Remove CarExploded method from invocation list.
+            c1.Exploded -= d;
+            Console.WriteLine("\n***** Speeding up *****");
+            for (int i = 0; i < 6; i++)
+            {
+                c1.Accelerate(20);
+            }
+
+            Console.ReadLine();            
+        }
+
+        static void CarAboutToBlow(string msg)
+        {
+            Console.WriteLine(msg);
+        }
+
+        static void CarIsAlmostDoomed(string msg)
+        {
+            Console.WriteLine("=> Critical Message from Car: {0}", msg);
+        }
+
+        static void CarExploded(string msg)
+        {
+            Console.WriteLine(msg);
+        }
+    }
+}
